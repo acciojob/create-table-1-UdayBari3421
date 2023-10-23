8c6c0bcb25a930b86546ae2edc4232e62cdb1386
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-23 12:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-function insert_Row() {
-    const numRows = parseInt(prompt("Input number of rows:"));
-    const table = document.getElementById("sampleTable");
-    table.innerHTML =  `
-		<tr>
-			<td>New Cell1</td> 
-			<td>New Cell2</td>
-		</tr> 
-		<tr>
-			<td>Row1 cell1</td> 
-			<td>Row1 cell2</td>
-		</tr> 
-		<tr>
-			<td>Row2 cell1</td> 
-			<td>Row2 cell2</td>
-		</tr>
-		
-  
-	`;
+const table = document.getElementById("sampleTable");
 
-	for (let i = 0; i < numRows; i++) {
+function insert_Row() {
+	for (let i = 0; i < 1; i++) {
         const row = table.insertRow(i);
         for (let j = 0; j < 2; j++) {
             const cell = row.insertCell(j);
